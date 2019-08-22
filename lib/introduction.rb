0fd7d19f9d9912877be4df71b375f9e341901d9b
Sarah H <sarahrose.henderson@gmail.com>
2019-08-22 14:14:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,9 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-introduction(sarah)
-end
+def introduction(sarah)
 puts "Hi, my name is #{name}"
-
+end
 
 
 
